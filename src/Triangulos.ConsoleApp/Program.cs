915849e65c6b9e89462c1e72d216cd89da2e1173
ISRAEL FANTONI <127950758+Israel-ux440@users.noreply.github.com>
2024-04-08 18:15:07 -0300

    Exercício de Lógica 2024 aula 21

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Informe os comprimentos dos três lados do triângulo:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+
+            Triangulo triangulo = new Triangulo(x, y, z);
+            triangulo.VerificarTipoTriangulo();
+
+            Console.ReadLine(); // Para manter a janela do console aberta
         }
     }
 }
+
---
+++ src/Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,40 @@
+﻿namespace Triangulos.ConsoleApp
+{
+    class Triangulo
+    {
+        public double lado1;
+        public double lado2;
+        public double lado3;
+
+        public Triangulo(double x, double y, double z)
+        {
+            lado1 = x;
+            lado2 = y;
+            lado3 = z;
+        }
+
+        public void VerificarTipoTriangulo()
+        {
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2)
+            {
+                if (lado1 == lado2 && lado2 == lado3)
+                {
+                    Console.WriteLine("É um triângulo equilátero.");
+                }
+                else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+                {
+                    Console.WriteLine("É um triângulo isósceles.");
+                }
+                else
+                {
+                    Console.WriteLine("É um triângulo escaleno.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Os valores informados não formam um triângulo.");
+            }
+        }
+    }
+}
+
